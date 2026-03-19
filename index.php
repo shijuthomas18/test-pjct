66472a8a8fca485a588913ad214b9bd1e2e2f226
shijuthomas18 <shijuthomas02@gmail.com>
2026-03-19 10:02:21 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Split Landing Page - Home Page - featured1</title>
+  </head>
+  <body>
+    <main class="container">
+      <section class="split left">
+        <h1>PlayStation 5.1 Featured branch</h1>
+        <a href="#" class="btn">Buy Nowwwwww</a>
+      </section>
+      <section class="split right">
+        <h1>Xbox Series X</h1>
+        <a href="#" class="btn">Buy Now</a>
+      </section>
+    </main>
+    <script src="script.js"></script>
+  </body>
+</html>
